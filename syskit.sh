ca55e18dab283ec6670ba896e5d81c34da16f2b7
ondombaemmanuel <ondombaemmanuel599@gmail.com>
2026-03-20 17:31:50 +0100

    Ajout du script principal syskit.sh (gestion de base des commandes)

--- syskit.sh
+++ syskit.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
 # Script principal SysKit
-usage() {
-    echo "Utilisation : $0 {inventory|backup|cleanup|report} [options]"
-    echo "Exemples :"
-    echo "  $0 inventory"
-    echo "  $0 backup /home/user/Documents"
-    echo "  $0 cleanup /tmp"
-    echo "  $0 report"
-    exit 1
-}
-
-if [ $# -lt 1 ]; then usage; fi
-
 COMMAND=$1
 shift
 
 case $COMMAND in
-    inventory) ./modules/inventory.sh ;;
+    inventory)
+        ./modules/inventory.sh
+        ;;
     backup)
-        if [ $# -lt 1 ]; then echo "Erreur : dossier manquant"; usage; fi
         ./modules/backup.sh "$1"
         ;;
     cleanup)
-        if [ $# -lt 1 ]; then echo "Erreur : dossier manquant"; usage; fi
         ./modules/cleanup.sh "$1"
         ;;
-    report) ./modules/report.sh ;;
-    *) echo "Commande inconnue : $COMMAND"; usage ;;
+    report)
+        ./modules/report.sh
+        ;;
+    *)
+        echo "Commande inconnue"
+        ;;
 esac 
 
